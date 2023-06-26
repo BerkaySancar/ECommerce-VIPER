f1d1ba464ead9d177c93fb340d5240f12c3d0fa8
Berkay Sancar <sancarberkay@gmail.com>
2023-06-26 15:45:08 +0300

    Import Firebase

--- ECommerce/Core/AppDelegate.swift
+++ ECommerce/Core/AppDelegate.swift
@@ -7,12 +7,15 @@
 
 import UIKit
 import SnapKit
+import FirebaseCore
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        FirebaseApp.configure()
         return true
     }
 
